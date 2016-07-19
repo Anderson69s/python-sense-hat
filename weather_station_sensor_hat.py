6c5de8b781511425aebe4ffba8692ea205d18a1f
Alexandre Perier-Muzet <andersflex69@gmail.com>
2016-07-19 11:20:14 +0200

    3rd
    add comments

--- weather_station_sensor_hat.py
+++ weather_station_sensor_hat.py
@@ -1,13 +1,14 @@
+# On importe les dofférentes libraries nécessaires.
 from sense_hat import SenseHat
-from random import randint
 import time
 import httplib, urllib
 import psutil
 from time import localtime, strftime
 
-sense = SenseHat()
-#sense.set_rotation(180)
+sense = SenseHat()#On démarre le shield
+#sense.set_rotation(180) #pour changer l'angle d'affichage
 
+#On définit des couleurs
 green = (0, 255, 0)
 red = (255, 0, 0)
 blue = (0, 0, 255)
@@ -15,14 +16,17 @@ yellow = (255, 255, 0)
 white = (255, 255, 255)
 none = (0, 0, 0)
 
+#On affiche un message de démarrage
 sense.show_message("Starting", text_colour=yellow, scroll_speed=0.05)
 sense.show_message("Anderson69s", text_colour=blue, back_colour=none, scroll_speed=0.05)
 sense.show_message("Raspberry", text_colour=green, back_colour=none, scroll_speed=0.05)
 sense.show_message("Sense Hat", text_colour=red, back_colour=none, scroll_speed=0.05)
 
+#On lance une boucle infinie pour faire notre script
 while True :
 
     for i in range (0, 1):
+        #On lit les valeurs différents capteurs
         humidity = sense.get_humidity()
         #print("Humidity: %s %%H" % humidity)
         sense_pressure = sense.get_pressure()
@@ -41,7 +45,8 @@ while True :
         #print mem_avail_mb
         cpu_temp = round(int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3,1)
         #print cpu_temp
-            
+        
+        #En fonction de la température lue, on change la couleur d'écriture
         if temp3 < 10 :
                #print "cas 1"
                color = white
@@ -57,7 +62,8 @@ while True :
         elif temp3 >= 35 :
                #print "cas 5"
                color = red
-               
+        
+        #On affiche les valeurs des capteurs sur la matrice       
         sense.show_message("T: %d" % temp3, text_colour=color, scroll_speed=0.08)
         sense.show_message("P: %d" % convert_pressure, text_colour=color, scroll_speed=0.08)
         sense.show_message("H: %d" % humidity, text_colour=color, scroll_speed=0.08)
@@ -65,7 +71,7 @@ while True :
         #sense.show_message("CPU Temp %sC" % cpu_temp, text_colour=color, scroll_speed=0.08)
         #sense.show_message("Free Ram %s" % mem_avail_mb, text_colour=color, scroll_speed=0.08)
         time.sleep(1)
-        
+    #On utilise httplib pour envoyer une requete à ThingSpeak en indiquant les paramètre et la clé API    
     headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
     conn = httplib.HTTPConnection("api.thingspeak.com:80")
     params = urllib.urlencode({'field1': convert_pressure,'field2': temp3,'field3': humidity,'field4': cpu_pc,'field5':cpu_temp,'field6': mem_avail_mb,'key':'VOTRE_CLE_THINGSPEAK'})
@@ -76,8 +82,10 @@ while True :
         #print response.status, response.reason
         data = response.read()
         conn.close()
+        #On affiche un message de succès si ThingSpeak répond
         sense.show_message("ThingSpeak OK", text_colour=green, scroll_speed=0.05)
         #print "connection ok"
     except:
+        #On affiche une erreur si ThingSpeak ne répond pas
         sense.show_message("ThingSpeak Error", text_colour=red, scroll_speed=0.05)
         print "connection error"
